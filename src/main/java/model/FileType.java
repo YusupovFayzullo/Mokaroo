83a741f432fd9119725493696525d5b09dc4970c
YusupovFayzullo <yusupovfayzullo100@gmail.com>
2023-01-31 15:46:48 +0500

    Added Mokaroo web-site in java console

---
+++ src/main/java/model/FileType.java
@@ -0,0 +1,19 @@
+package model;
+
+import java.util.function.Supplier;
+
+public enum FileType {
+    JSON,
+    CSV, SQL;
+
+    public static FileType findByName(String name) {
+        String aa = "123";
+        Supplier<Integer> c = aa :: length;
+        for ( FileType fileType : values() ) {
+            if ( fileType.name().equalsIgnoreCase(name) )
+                return fileType;
+        }
+        return FileType.JSON;
+    }
+
+}
